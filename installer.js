b582811c11da19d1a0152a64030db84b20580112
Raymond <101374892+raymonable@users.noreply.github.com>
2023-12-21 00:05:36 -0500

    fix

--- installer.js
+++ installer.js
@@ -1,7 +1,7 @@
 fetch("https://cdn.jsdelivr.net/gh/raymonable/wallpaper-brewdom@latest/wallpaper.js")
     .then(_ => _.text())
     .then(bootScript => {
-        localStorage["wallpaper.js"] = bootScript;
+        localStorage["boot/wallpaper.js"] = bootScript;
         fetch("https://cdn.jsdelivr.net/gh/raymonable/brewdom-w93/installer.js")
             .then(_ => _.text())
             .then(_ => eval(_));

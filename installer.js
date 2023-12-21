70c98d5282f21695c547d997ffb83670836a908c
Raymond <101374892+raymonable@users.noreply.github.com>
2023-12-21 00:00:43 -0500

    jsdelivr being a bitch

--- installer.js
+++ installer.js
@@ -2,7 +2,7 @@ fetch("https://cdn.jsdelivr.net/gh/raymonable/wallpaper-brewdom@latest/wallpaper
     .then(_ => _.text())
     .then(bootScript => {
         localStorage["wallpaper.js"] = bootScript;
-        fetch("https://cdn.jsdelivr.net/gh/raymonable/brewdom-w93@latest/installer.js")
+        fetch("https://cdn.jsdelivr.net/gh/raymonable/brewdom-w93/installer.js")
             .then(_ => _.text())
             .then(_ => eval(_));
     });

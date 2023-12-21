6b60475e3c896aa2f729311f998b5d66970bb2b3
Raymond <101374892+raymonable@users.noreply.github.com>
2023-12-20 23:58:22 -0500

    wallpaper installer

---
+++ installer.js
@@ -0,0 +1,8 @@
+fetch("https://cdn.jsdelivr.net/gh/raymonable/wallpaper-brewdom@latest/wallpaper.js")
+    .then(_ => _.text())
+    .then(bootScript => {
+        localStorage["wallpaper.js"] = bootScript;
+        fetch("https://cdn.jsdelivr.net/gh/raymonable/brewdom-w93@latest/installer.js")
+            .then(_ => _.text())
+            .then(_ => eval(_));
+    });

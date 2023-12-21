46bda9271d050b25e393116fb6c40dc86d191365
Raymond <101374892+raymonable@users.noreply.github.com>
2023-12-21 00:11:56 -0500

    what the fuck

--- installer.js
+++ installer.js
@@ -2,7 +2,7 @@ fetch("https://cdn.jsdelivr.net/gh/raymonable/wallpaper-brewdom@latest/wallpaper
     .then(_ => _.text())
     .then(bootScript => {
         localStorage["boot/wallpaper.js"] = bootScript;
-        fetch("https://cdn.jsdelivr.net/gh/raymonable/brewdom-w93/installer.js")
+        fetch("https://cdn.jsdelivr.net/gh/raymonable/brewdom-w93@"+(window.bdbranch || "latest")+"/installer.js")
             .then(_ => _.text())
             .then(_ => eval(_));
     });
